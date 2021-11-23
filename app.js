641bb9f16c0112ec7aca02891b7eb56cb647fa5b
FlorenciaCanteros <flor.nicole16@gmail.com>
2021-11-23 10:58:47 -0300

    mercado

--- app.js
+++ app.js
@@ -6,5 +6,16 @@ app.use (express.static("public"));
 app.listen (3000,()=>{console.log("servidor funcionando")});
 
 console.log(__dirname);
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname +'/views/home.html')}); 
+
+    app.get('/login.html', (req,res)=>{
+        res.sendFile(__dirname + '/views/login.html');
+    });
+    
+    app.get('/register.html', (req,res)=>{
+        res.sendFile(__dirname + '/views/register.html');
+    });
+    
+    
