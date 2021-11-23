cfb016dada7de876bebfcf06c583ec27c26a8be9
FlorenciaCanteros <flor.nicole16@gmail.com>
2021-11-23 11:11:41 -0300

    commit

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 
 app.use (express.static("public"));
 
-app.listen (3000,()=>{console.log("servidor funcionando")});
+app.listen (process.env.PORT ||
+    3000,()=>{console.log("servidor funcionando")});
 
 console.log(__dirname);
 
